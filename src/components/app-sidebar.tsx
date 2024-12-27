2c4327c8b3db9cc49d16e81d90a10deff3369bb8
luis amorim <luisfelipeamorim@hotmail.com>
2024-12-27 15:32:52 -0300

    Update AppSidebar to include collapsible prop for improved sidebar behavior

--- src/components/app-sidebar.tsx
+++ src/components/app-sidebar.tsx
@@ -22,7 +22,7 @@ export function AppSidebar({ ...props }: AppSidebarProps) {
 
 	return (
 		<>
-			<Sidebar {...props}>
+			<Sidebar {...props} collapsible={"none"}>
 				<SidebarHeader>
 					<SidebarHeaderItem />
 				</SidebarHeader>
